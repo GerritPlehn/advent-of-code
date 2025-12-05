c93b5b70d789c3f1be7f993231be16fee2582c52
Gerrit Plehn <gerrit@plehn.me>
2025-12-05 16:56:19 +0100

    finish day 1 task 2

--- 2025/day01/day01-2.ts
+++ 2025/day01/day01-2.ts
@@ -3,22 +3,32 @@
 import { readFile } from "node:fs/promises";
 
 async function main() {
-	const input = (await readFile("2025/day01/input.sample.txt", "utf8")).split(
+	const input = (await readFile("2025/day01/input.txt", "utf8")).split(
 		"\n",
 	);
 	let counter = 50;
 	const steps = 100;
 
 	function left(n: number) {
-		const fullRotations = Math.floor(n / steps);
+		const startPoint = counter;
+		const fullRotations = Math.floor(Math.abs(n) / steps);
 
 		counter -= n;
 		counter = counter % steps;
 		if (counter < 0) {
 			counter = steps + counter;
 		}
-		if (zeroCrossings) console.log(`crossed zero ${zeroCrossings} times`);
-		return fullRotations;
+		let zeroCrossings = 0
+		if (n < 0 && counter < startPoint) {
+			// we rotated right and ended up with a smaller number
+			zeroCrossings = 1
+		}
+		if (n > 0 && counter > startPoint) {
+			// we rotated left and ended up with a larger number
+			zeroCrossings = 1
+		}
+		if (startPoint === 0 || counter === 0) return fullRotations
+		return fullRotations+zeroCrossings;
 	}
 
 	function right(n: number) {
@@ -27,7 +37,7 @@ async function main() {
 
 	console.log(`The dial starts by pointing at ${counter}.`);
 	let zeroCrossings = 0;
-
+	let zeroEndings = 0;
 	for (const row of input) {
 		const matches = row.match(/(L|R)([0-9]+)/);
 		const direction = matches?.at(1);
@@ -35,11 +45,16 @@ async function main() {
 		if (!direction || !amount) {
 			continue;
 		}
-		if (direction === "L") zeroCrossings += left(amount);
-		if (direction === "R") zeroCrossings += right(amount);
-		// if (counter === 0) zeroCrossings++;
+		let rotationZeroCrossings = 0;
+		if (direction === "L") rotationZeroCrossings = left(amount);
+		if (direction === "R") rotationZeroCrossings = right(amount);
+		if (counter === 0) zeroEndings += 1
 		console.log(`The dial is rotated ${row} to point at ${counter}.`);
+		if (rotationZeroCrossings) {
+			console.log(`it crossed zero ${rotationZeroCrossings} times`);
+		}
+		zeroCrossings += rotationZeroCrossings;
 	}
-	console.log(`The password is ${zeroCrossings}`);
+	console.log(`The password is ${zeroEndings+zeroCrossings}`);
 }
 await main();
